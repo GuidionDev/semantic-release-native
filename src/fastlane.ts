3220be7d96c6ffedfbce8f99d28b5e7303a5a4cb
Yigit Sozer <sozer.work@gmail.com>
2024-07-04 13:27:21 +0200

    chore(GDD-5510): fix the fastlane review directory

--- src/fastlane.ts
+++ src/fastlane.ts
@@ -4,8 +4,8 @@ import { mkdir } from 'fs/promises';
 import { Context } from './interfaces';
 
 export const writeNotesToFastlane = async (osPath: string, releaseNotes: string, log: Context['logger']['log']) => {
-  const metaDataPath = path.join(osPath, 'fastlane', 'metadata', 'review_information');
-  const notesPath = path.join(metaDataPath, 'notes.txt');
+  const metaDataPath = path.join(osPath, 'fastlane', 'metadata', 'nl-NL');
+  const notesPath = path.join(metaDataPath, 'release_notes.txt');
   log('[📝] Writing release notes to', notesPath);
 
   await mkdir(metaDataPath, { recursive: true });
